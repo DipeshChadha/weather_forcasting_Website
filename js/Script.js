85338cfc73981e8182bf9e0aff6987d9eec78bbd
DipeshChadha <chadhadipesh@gmail.com>
2025-06-05 22:25:47 +0530

    Add repository

---
+++ js/Script.js
@@ -0,0 +1,21 @@
+async function fetchData() 
+{
+ await fetch('http://api.weatherapi.com/v1/current.json?key=735cabe3c3334eac91155206250506&q=Mohali&aqi=no')
+  .then(response =>{
+//response.text()
+   return response.json(); 
+}).then((response) => { 
+  console.log("Data fetched successfully",response);
+  console.log("Data fetched successfully",response.current.temp_c);
+
+  var city=document.getElementById("city");
+  var temp=document.getElementById("temp");
+  var condition=document.getElementById("condition");
+
+
+  city.innerHTML=response.location.name+ ", " + response.location.country;
+  temp.innerHTML=response.current.temp_c + "°C";
+    condition.innerHTML=response.current.condition.text;
+})
+  };
+fetchData();
